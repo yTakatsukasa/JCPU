ec6f9217dcb86e04acdb8bcaf9e4f37cea158107
Yutetsu TAKATSUKASA <y.takatsukasa@gmail.com>
2013-06-22 13:08:27 +0900

    add ifdef for c++03

--- include/jcpu_internal.h
+++ include/jcpu_internal.h
@@ -2,8 +2,14 @@
 #define JCPU_INTERNAL_H
 #include <iostream>
 #include <cstdlib>
+
+#if __cplusplus >= 201103L
 #define JCPU_OVERRIDE override
 #define JCPU_NULLPTR  nullptr
+#else
+#define JCPU_OVERRIDE 
+#define JCPU_NULLPTR  NULL
+#endif
 
 #define jcpu_assert(cond) do{if(!(cond)){std::cerr << "Assert(" << #cond << ") failed in " << __FILE__ << __LINE__ << std::endl; std::abort();}} while(false)
 namespace jcpu{
